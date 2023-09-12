0f4f923e7111825cc05ae83353551b12120e4ffb
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-09-12 10:12:53 -0300

    cria input para Login

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,23 @@
+import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+
+interface InputIndexProps{
+    label:string 
+    type:string
+    w:string
+    placeholder:string
+}
+
+export function InputIndex({label,type,w,placeholder}:InputIndexProps) {
+    return (
+        <FormControl
+        w={w}
+        >
+            <FormLabel
+            fontFamily="montserrat"
+            fontWeight={"bold"}
+            fontSize={"20px"}
+            >{label}</FormLabel>
+            <Input type={type} placeholder={placeholder}/>
+        </FormControl>
+    )
+}
