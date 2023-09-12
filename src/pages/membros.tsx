c75743325194cbd1cc8b4f0a086549e1acf4f5d0
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-09-12 18:11:01 -0300

    adiciona tabeela de membros

--- src/pages/membros.tsx
+++ src/pages/membros.tsx
@@ -1,3 +1,4 @@
+import { TableIndex } from "@/components/Table";
 import { TitleIndex } from "@/components/Title";
 import { Stack } from "@chakra-ui/react";
 
@@ -5,8 +6,19 @@ import { Stack } from "@chakra-ui/react";
 
 export default function Membros(){
     return(
-        <Stack>
-            <TitleIndex text={"Membros"} color={"black"} size={"64px"}/>
+        <Stack
+        align="center"
+        >
+            <Stack>
+                <TitleIndex text={"Membros"} color={"black"} size={"64px"}/>
+            </Stack>
+            <Stack
+            py={10}
+            spacing={10}
+            >
+           <TableIndex nome={"Pedro"} email={"@gmail"} niver={"28/09/2000"} cargo={"Asessor"}/>
+            <TableIndex nome={"Pedro"} email={"@gmail"} niver={"28/09/2000"} cargo={"Asessor"}/>
+            </Stack>
         </Stack>
     )
 }
