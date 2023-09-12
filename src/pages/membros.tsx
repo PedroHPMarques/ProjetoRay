c534f18e99a969de20a9ebae8bd1cacec33dac79
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-09-12 10:14:20 -0300

    cria pagina membros

---
+++ src/pages/membros.tsx
@@ -0,0 +1,12 @@
+import { TitleIndex } from "@/components/Title";
+import { Stack } from "@chakra-ui/react";
+
+
+
+export default function Membros(){
+    return(
+        <Stack>
+            <TitleIndex text={"Membros"} color={"black"} size={"64px"}/>
+        </Stack>
+    )
+}
